d3e42628f835161a695139e68809b22082629aa5
TalyssonG <lopestalysson99@gmail.com>
2021-01-02 13:59:44 -0300

    Atributos personalizados - parte 1

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+let lista = document.getElementById('lista');
+
+let num = parseInt(lista.getAttribute('num'));
+
+let conteudo = ''
+
+for(let i = 1; i <= num; i++){
+    conteudo += "<li>" + i + "</li>";
+
+}
+
+lista.innerHTML = conteudo;
